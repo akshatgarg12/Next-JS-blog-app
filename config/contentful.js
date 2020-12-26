25774b029a5b188225be777b518318ed9657071c
akshatgarg12 <akshatarungarg78@gmail.com>
2020-12-26 13:58:14 +0530

    env variable fix

--- config/contentful.js
+++ config/contentful.js
@@ -1,9 +1,9 @@
 const contentful = require('contentful');
 
 const client = contentful.createClient({
-  space:process.env.SPACE_ID,
+  space:process.env.NEXT_PUBLIC_SPACE_ID,
   environment:'master', // defaults to 'master' if not set
-  accessToken:process.env.ACCESS_TOKEN
+  accessToken:process.env.NEXT_PUBLIC_ACCESS_TOKEN
 })
 
 module.exports = client;
